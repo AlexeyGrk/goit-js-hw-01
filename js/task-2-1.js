49d1d70504a0be1ff0c495df52e0e64470cb2f22
AlexeyGrk <alexgmirko@gmail.com>
2020-09-26 12:33:14 +0300

    task-2....n

--- js/task-2-1.js
+++ js/task-2-1.js
@@ -2,8 +2,8 @@ const getItemsString = function (array) {
   "use strict";
   // Write code under this line
   let result = "";
-  for (let i = 0; i < array.lenght; i += 1) {
-    result += `${[i + 1]} - ${array[i]}`;
+  for (let i = 0; i < array.length; i += 1) {
+    result += `${[i + 1]} - ${array[i]}\n`;
   }
   return result;
 };
@@ -19,10 +19,11 @@ console.log(getItemsString(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]));
 '
 */
 
-console.log(getItemsString([51, 10, 15]));
+//console.log(getItemsString([5, 10, 15]));
 /*
 '1 - 5
 2 - 10
 3 - 15
 '
 */
+//
