d53db8ab7472a7a19d2acbfcffae9637e90f056b
AlexeyGrk <alexgmirko@gmail.com>
2020-11-08 01:15:43 +0200

    добавляет 6-1 и начинает 6-2

---
+++ js/task-6-2.js
@@ -0,0 +1,9 @@
+numbers = [1, 3, 4, 5];
+
+const fitelrNumbers = numbers.filter(function (number) {
+    return number < 5
+}); 
+console.log(fitelrNumbers)
+
+
+console.log(fitelrNumbers === numbers)
